05e69b6177d8e38c3562c6d0db00d7c8a7400c66
Hasegos <csw020106@naver.com>
2025-08-19 00:11:32 +0900

    FIX SavedRepo entity 필드 추가
    SavedRepo 엔터티 필요한 필드 추가 중
    추후에 추가할 예정

--- src/main/java/io/github/repoboard/model/SavedRepo.java
+++ src/main/java/io/github/repoboard/model/SavedRepo.java
@@ -1,4 +1,25 @@
 package io.github.repoboard.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "saved_repo")
 public class SavedRepo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
