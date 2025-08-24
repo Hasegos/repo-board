4a66fc179f38673f057864ef2d0d186c43cfc063
Hasegos <csw020106@naver.com>
2025-08-24 23:30:16 +0900

    FIX ProfileDTO DTO 필드값 추가
    오픈프로필 필요한 DTO 필드값 추가

--- src/main/java/io/github/repoboard/dto/ProfileDTO.java
+++ src/main/java/io/github/repoboard/dto/ProfileDTO.java
@@ -1,4 +1,17 @@
 package io.github.repoboard.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProfileDTO {
+
+    private Long id;
+    private String nickname;
+    private String repositoryUrl;
+    private String stacks;
+    private String selfInfo;
+    private String experience;
+    private String imageUrl;
 }
