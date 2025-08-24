99691e5e6ffad81f743fe34e509b29041e723fb1
Hasegos <csw020106@naver.com>
2025-08-24 23:32:50 +0900

    FIX ProfileRepository 필요한 메서드 추가
    조회용, 불일치용 메서드 추가

--- src/main/java/io/github/repoboard/repository/ProfileRepository.java
+++ src/main/java/io/github/repoboard/repository/ProfileRepository.java
@@ -4,6 +4,13 @@ import io.github.repoboard.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
+    Optional<Profile> findByUserId(Long userId);
+    boolean existsByNickname(String nickname);
+    boolean existsByUserId(Long userId);
+    boolean existsByNicknameAndUserIdNot(String nickname, Long userId);
 }
