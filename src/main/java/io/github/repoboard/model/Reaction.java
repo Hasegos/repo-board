27a6e45c72eca1045973ee0113f8a08e286741dd
Hasegos <csw020106@naver.com>
2025-08-24 23:28:30 +0900

    FIX timestamptz 삭제
    postgresql 설정상 안맞아서
    columnDefinition "timestamptz" 삭제

--- src/main/java/io/github/repoboard/model/Reaction.java
+++ src/main/java/io/github/repoboard/model/Reaction.java
@@ -39,6 +39,6 @@ public class Reaction {
     private ReactionType reactionType;
 
     @CreationTimestamp
-    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "timestamptz")
+    @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
 }
