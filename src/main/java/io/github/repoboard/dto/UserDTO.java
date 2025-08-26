3bf201d57cac34c28b4b2dd5178a3ebb50ee78ab
Hasegos <csw020106@naver.com>
2025-08-26 18:38:56 +0900

    FIX UserDTO Validation추가
    비밀번호, 아이디 validation 추가

--- src/main/java/io/github/repoboard/dto/UserDTO.java
+++ src/main/java/io/github/repoboard/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package io.github.repoboard.dto;
 
+import io.github.repoboard.common.validation.annotation.ValidPassword;
+import io.github.repoboard.common.validation.annotation.ValidUsername;
 import io.github.repoboard.model.enums.UserProvider;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -10,10 +12,16 @@ import lombok.Setter;
 public class UserDTO {
 
     @NotBlank(message = "아이디는 필수 입력입니다.")
+    @ValidUsername
     private String username;
 
     @NotBlank(message = "비밀번호는 필수 입력입니다.")
+    @ValidPassword
     private String password;
+
+    @ValidPassword
+    private String passwordConfirm;
+
     private String role;
     private UserProvider provider;
     private String providerId;
