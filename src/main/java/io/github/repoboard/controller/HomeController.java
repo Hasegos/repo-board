dc2918586e41764beabd736b3e93f6dc1c5ee9e4
Hasegos <csw020106@naver.com>
2025-09-02 16:17:37 +0900

    FIX HomeController API 호출 추가
    기존 테스트용 API 호출 이쪽으로 이동

--- src/main/java/io/github/repoboard/controller/HomeController.java
+++ src/main/java/io/github/repoboard/controller/HomeController.java
@@ -1,14 +1,21 @@
 package io.github.repoboard.controller;
 
+import io.github.repoboard.dto.GithubRepoDTO;
+import io.github.repoboard.model.User;
 import io.github.repoboard.security.core.CustomUserPrincipal;
+import io.github.repoboard.service.GitHubApiService;
 import io.github.repoboard.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -17,13 +24,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     private final UserService userService;
+    private final GitHubApiService gitHubApiService;
 
     @GetMapping
     public String showhome(@AuthenticationPrincipal CustomUserPrincipal principal,
+                           @RequestParam(required = false, defaultValue = "java") String language,
+                           @RequestParam(defaultValue = "0") int page,
                            Model model){
         if(principal != null){
             model.addAttribute("user", principal.getUser());
         }
+        Pageable finalPageable = PageRequest.of(page, 50);
+        Page<GithubRepoDTO> repoPage = gitHubApiService.searchPublicRepos(language,finalPageable);
+        model.addAttribute("repoPage", repoPage);
+        model.addAttribute("currentLanguage", language);
+
         return "home";
+    }
+
+    @GetMapping("/more")
+    public String loadMoreRepositories(@RequestParam(required = false, defaultValue = "java") String language,
+                                       @RequestParam(defaultValue = "1") int page,
+                                       Model model){
+
+        Pageable pageable = PageRequest.of(page, 50);
+        Page<GithubRepoDTO> repoPage = gitHubApiService.searchPublicRepos(language,pageable);
+        model.addAttribute("repoPage", repoPage);
+        model.addAttribute("currentLanguage", language);
+
+        return "fragments/repo-card :: repo-cards";
     }
 }
