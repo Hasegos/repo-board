ba39dbc6d4d5c73a2b3d58651450877681bb1896
Hasegos <csw020106@naver.com>
2025-09-03 23:59:59 +0900

    FIX HomeController API 변동 및 f5 새로고침 / 버튼 새로고침 분기
    f5 새로고침 기존내용유지 / 버튼 새로고침 다른쿼리문 조회

--- src/main/java/io/github/repoboard/controller/HomeController.java
+++ src/main/java/io/github/repoboard/controller/HomeController.java
@@ -4,6 +4,8 @@ import io.github.repoboard.dto.GithubRepoDTO;
 import io.github.repoboard.security.core.CustomUserPrincipal;
 import io.github.repoboard.service.GitHubApiService;
 import io.github.repoboard.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -13,11 +15,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+
 @Controller
-@RequestMapping("/")
 @RequiredArgsConstructor
 @Slf4j
 public class HomeController {
@@ -25,29 +27,39 @@ public class HomeController {
     private final UserService userService;
     private final GitHubApiService gitHubApiService;
 
-    @GetMapping
+    @GetMapping("/")
     public String showhome(@AuthenticationPrincipal CustomUserPrincipal principal,
                            @RequestParam(value = "language", required = false, defaultValue = "java") String language,
+                           @RequestParam(value = "refresh", required = false, defaultValue = "false") boolean refresh,
                            @RequestParam(defaultValue = "0") int page,
-                           Model model){
+                           HttpServletRequest request,
+                           HttpServletResponse response,
+                           Model model) throws IOException {
         if(principal != null){
             model.addAttribute("user", principal.getUser());
         }
+
+        if (refresh) {
+            response.sendRedirect("/?language=" + language + "&page=" + page);
+            return null;
+        }
+
         Pageable finalPageable = PageRequest.of(page, 50);
-        Page<GithubRepoDTO> repoPage = gitHubApiService.searchPublicRepos(language,finalPageable);
+        Page<GithubRepoDTO> repoPage = gitHubApiService.fetchRepos(language,finalPageable, refresh);
         model.addAttribute("repoPage", repoPage);
         model.addAttribute("currentLanguage", language);
 
         return "home";
     }
 
-    @GetMapping("/more")
+    @GetMapping("/api/repos")
     public String loadMoreRepositories(@RequestParam(required = false, defaultValue = "java") String language,
+                                       @RequestParam(required = false, defaultValue = "false") boolean refresh,
                                        @RequestParam(defaultValue = "1") int page,
                                        Model model){
 
         Pageable pageable = PageRequest.of(page, 50);
-        Page<GithubRepoDTO> repoPage = gitHubApiService.searchPublicRepos(language,pageable);
+        Page<GithubRepoDTO> repoPage = gitHubApiService.fetchRepos(language,pageable,refresh);
         model.addAttribute("repoPage", repoPage);
         model.addAttribute("currentLanguage", language);
 
