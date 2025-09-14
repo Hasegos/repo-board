b8f24fd732c4d6800bf679c4b6b2f26ad2ea2e36
Hasegos <csw020106@naver.com>
2025-09-15 07:51:47 +0900

    FIX savedRepoDTO 수정
    저장한 레포 DTO 정보 필드 추가

--- src/main/java/io/github/repoboard/dto/SavedRepoDTO.java
+++ src/main/java/io/github/repoboard/dto/SavedRepoDTO.java
@@ -1,4 +1,30 @@
 package io.github.repoboard.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SavedRepoDTO {
+
+    private Integer repoGithubId;
+    private String name;
+    private String htmlUrl;
+    private String description;
+    private String languageMain;
+    private Integer stars;
+    private Integer forks;
+    private String readmeExcerpt;
+    private String note;
+    private Instant updatedAt;
+
+    private String ownerLogin;
+    private String ownerAvatarUrl;
+    private String ownerHtmlUrl;
 }
