bd9578bbea051df4877cdfe3851f72442eb3156d
Hasegos <csw020106@naver.com>
2025-09-15 16:03:15 +0900

    FIX SavedRepoController 수정
    readme 비동기 컨트롤러 추가

--- src/main/java/io/github/repoboard/controller/SavedRepoController.java
+++ src/main/java/io/github/repoboard/controller/SavedRepoController.java
@@ -3,9 +3,11 @@ package io.github.repoboard.controller;
 import io.github.repoboard.model.SavedRepo;
 import io.github.repoboard.model.User;
 import io.github.repoboard.security.core.CustomUserPrincipal;
+import io.github.repoboard.service.GitHubApiService;
 import io.github.repoboard.service.SavedRepoService;
 import io.github.repoboard.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +22,7 @@ public class SavedRepoController {
 
     private final UserService userService;
     private final SavedRepoService savedRepoService;
+    private final GitHubApiService gitHubApiService;
 
     @GetMapping
     public String showSavedRepo(@AuthenticationPrincipal CustomUserPrincipal principal,
@@ -31,6 +34,12 @@ public class SavedRepoController {
         model.addAttribute("savedRepos", savedRepos);
 
         return "repository/saved";
+    }
+
+    @GetMapping("/{id}/readme")
+    public ResponseEntity<String> getReadmeByRepoId(@PathVariable Long id){
+        String readme = gitHubApiService.getReadmeById(id);
+        return ResponseEntity.ok(readme);
     }
 
     @PostMapping
