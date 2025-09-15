0a386a85a92ddd1775fd64d710db509c67c6419c
Hasegos <csw020106@naver.com>
2025-09-15 11:16:40 +0900

    FIX SavedRepoController 수정
    post 저장 요청 수정중

--- src/main/java/io/github/repoboard/controller/SavedRepoController.java
+++ src/main/java/io/github/repoboard/controller/SavedRepoController.java
@@ -1,20 +1,15 @@
 package io.github.repoboard.controller;
 
-import io.github.repoboard.dto.GithubRepoDTO;
 import io.github.repoboard.model.SavedRepo;
 import io.github.repoboard.model.User;
 import io.github.repoboard.security.core.CustomUserPrincipal;
 import io.github.repoboard.service.SavedRepoService;
 import io.github.repoboard.service.UserService;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,17 +33,12 @@ public class SavedRepoController {
         return "repository/saved";
     }
 
-
     @PostMapping
     public String saveRepo(@AuthenticationPrincipal CustomUserPrincipal principal,
-                           @RequestParam Long id,
-                           HttpSession session){
-        List<GithubRepoDTO> cached = (List<GithubRepoDTO>) session.getAttribute("cachedRepos");
-        GithubRepoDTO repo = cached.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("해당 레포 없음"));
+                           @RequestParam("id") Long id){
 
+        User user = userService.findByUserId(principal.getUser().getId());
+        savedRepoService.savedRepoById(id, user);
 
         return "redirect:/users/saved/repos";
     }
