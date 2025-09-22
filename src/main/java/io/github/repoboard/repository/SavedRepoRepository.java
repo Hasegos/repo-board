3bf975144a0235de44c5413d61db2ba94396bc0f
Hasegos <csw020106@naver.com>
2025-09-22 22:28:19 +0900

    FIX SavedRepoRepository 파일 수정
    조회용 메서드 추가 (핀여부 / 언어 필터 여부)

--- src/main/java/io/github/repoboard/repository/SavedRepoRepository.java
+++ src/main/java/io/github/repoboard/repository/SavedRepoRepository.java
@@ -1,16 +1,23 @@
-    package io.github.repoboard.repository;
+package io.github.repoboard.repository;
 
-    import io.github.repoboard.model.SavedRepo;
-    import org.springframework.data.jpa.repository.JpaRepository;
-    import org.springframework.stereotype.Repository;
+import io.github.repoboard.model.SavedRepo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-    import java.util.List;
-    import java.util.Optional;
+import java.util.Optional;
 
-    @Repository
-    public interface SavedRepoRepository extends JpaRepository<SavedRepo, Long> {
-        Optional<SavedRepo> findByRepoGithubIdAndUserId(Long repoGithubId, Long userId);
-        List<SavedRepo> findAllByUserId(Long userId);
-        void deleteByRepoGithubIdAndUserId(Long repoGithubId, Long userId);
-        boolean existsByRepoGithubIdAndUserId(Long repoGithubId, Long userId);
-    }
+@Repository
+public interface SavedRepoRepository extends JpaRepository<SavedRepo, Long> {
+    Optional<SavedRepo> findByRepoGithubIdAndUserId(Long repoGithubId, Long userId);
+    void deleteByRepoGithubIdAndUserId(Long repoGithubId, Long userId);
+    boolean existsByRepoGithubIdAndUserId(Long repoGithubId, Long userId);
+
+    Page<SavedRepo> findAllByUserId(Long userId, Pageable pageable);
+    Page<SavedRepo> findAllByUserIdAndLanguageMainIgnoreCase(Long userId, String language, Pageable pageable);
+    Page<SavedRepo> findAllByUserIdAndIsPinnedTrue(Long userId, Pageable pageable);
+    Page<SavedRepo> findAllByUserIdAndIsPinnedTrueAndLanguageMainIgnoreCase(Long userId,String language, Pageable pageable);
+    Page<SavedRepo> findAllByUserIdAndIsPinnedFalse(Long userId, Pageable pageable);
+    Page<SavedRepo> findAllByUserIdAndIsPinnedFalseAndLanguageMainIgnoreCase(Long userId, String language, Pageable pageable);
+}
