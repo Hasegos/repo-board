5eeb054231db1824c582c3a435b05e6be1b7a513
Hasegos <csw020106@naver.com>
2025-09-29 22:33:06 +0900

    FIX SearchController 파일 수정
    "/users" 엔드포인트 추가 (유저 정보 조회용)

--- src/main/java/io/github/repoboard/controller/SearchController.java
+++ src/main/java/io/github/repoboard/controller/SearchController.java
@@ -1,8 +1,15 @@
 package io.github.repoboard.controller;
 
 import io.github.repoboard.dto.github.GithubRepoDTO;
+import io.github.repoboard.model.Profile;
+import io.github.repoboard.model.SavedRepo;
+import io.github.repoboard.model.User;
 import io.github.repoboard.security.core.CustomUserPrincipal;
 import io.github.repoboard.service.GitHubApiService;
+import io.github.repoboard.service.ProfileService;
+import io.github.repoboard.service.SavedRepoService;
+import io.github.repoboard.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,13 +22,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/search/repositories")
+@RequestMapping("/search")
 @RequiredArgsConstructor
 public class SearchController {
 
     private final GitHubApiService gitHubApiService;
+    private final UserService userService;
+    private final ProfileService profileService;
+    private final SavedRepoService savedRepoService;
 
-    @GetMapping
+    @GetMapping("/repositories")
     public String showSearch(@AuthenticationPrincipal CustomUserPrincipal principal,
                              @RequestParam("q") String search,
                              @RequestParam(defaultValue = "0") int page,
@@ -42,7 +52,7 @@ public class SearchController {
         return "search/search";
     }
 
-    @GetMapping("/api/repos")
+    @GetMapping("/repositories/api/repos")
     public String loadMoreRepositories(@RequestParam(defaultValue = "1") int page,
                                        @RequestParam("sort") String sort,
                                        @RequestParam("q") String search,
@@ -51,5 +61,33 @@ public class SearchController {
         Page<GithubRepoDTO> repoPage = gitHubApiService.fetchReposByQuery(search, pageable, sort);
         model.addAttribute("repoPage", repoPage);
         return "fragments/repo_card :: repo-cards";
+    }
+
+    @GetMapping("/users")
+    public String showSearchUsers(@AuthenticationPrincipal CustomUserPrincipal principal,
+                                  @RequestParam(required = false) String language,
+                                  @RequestParam(defaultValue = "recent") String sort,
+                                  @RequestParam(defaultValue = "0") int page,
+                                  @RequestParam(defaultValue = "6") int size,
+                                  @RequestParam("q") String query,
+                                  Model model){
+
+        User user = userService.findByUsername(query)
+                .orElseThrow(() -> new EntityNotFoundException("해당 유저는 존재하지않습니다."));
+
+        Profile profile = profileService.findProfileByUserId(user.getId())
+                .orElseThrow(() -> new EntityNotFoundException("해당 유저의 프로필이 존재하지않습니다."));
+
+        Pageable pageable = PageRequest.of(page,size);
+        Page<SavedRepo> savedRepo = savedRepoService.findAllSavedRepos(user.getId(), language, sort, pageable);
+
+        model.addAttribute("user", principal.getUser());
+        model.addAttribute("savedRepos", savedRepo);
+        model.addAttribute("profile", profile);
+        model.addAttribute("sort", sort);
+        model.addAttribute("language", language);
+        model.addAttribute("query", query);
+
+        return "search/search-user";
     }
 }
