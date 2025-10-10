149d32a0bb70c86ddce60c29309d69d79454ef3f
Hasegos <csw020106@naver.com>
2025-10-10 23:17:31 +0900

    FIX RepoBoardApplication 파일 수정
    @EnableScheduling 추가 (자동 삭제를 위해)

--- src/main/java/io/github/repoboard/RepoBoardApplication.java
+++ src/main/java/io/github/repoboard/RepoBoardApplication.java
@@ -2,9 +2,9 @@ package io.github.repoboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAspectJAutoProxy(exposeProxy = true)
+@EnableScheduling
 @SpringBootApplication
 public class RepoBoardApplication {
 
