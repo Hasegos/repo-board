16483337c87e3ae7c25eadc49b62af4f4b505682
Hasegos <csw020106@naver.com>
2025-10-13 19:33:03 +0900

    DEL 불필요한 클래스 파일 삭제
    사용안하는 클래스 파일 삭제

--- src/main/java/io/github/repoboard/dto/request/ReactionDTO.java
+++
@@ -1,4 +0,0 @@
-package io.github.repoboard.dto.request;
-
-public class ReactionDTO {
-}
--- src/main/java/io/github/repoboard/model/enums/ReactionType.java
+++
@@ -1,6 +0,0 @@
-package io.github.repoboard.model.enums;
-
-public enum ReactionType {
-    LIKE,
-    FOLLOW
-}
