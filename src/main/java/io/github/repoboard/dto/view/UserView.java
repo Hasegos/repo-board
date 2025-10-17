08882859f2d294faf4eb7de0af132d58889339b1
Hasegos <csw020106@naver.com>
2025-10-18 00:23:31 +0900

    FIX UserView 파일 수정
    user에 대한 정보 추가

--- src/main/java/io/github/repoboard/dto/view/UserView.java
+++ src/main/java/io/github/repoboard/dto/view/UserView.java
@@ -2,8 +2,11 @@ package io.github.repoboard.dto.view;
 
 import io.github.repoboard.model.User;
 import io.github.repoboard.model.enums.UserRoleType;
+import io.github.repoboard.model.enums.UserStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.Instant;
 
 @Getter
 @AllArgsConstructor
@@ -13,6 +16,9 @@ public class UserView {
     private final UserRoleType role;
     private final String provider;
     private final ProfileFullView profile;
+    private final UserStatus status;
+    private final Instant createdAt;
+    private final Instant updatedAt;
 
     public static UserView from(User user){
         if(user == null) return null;
@@ -20,7 +26,10 @@ public class UserView {
                 user.getUsername(),
                 user.getRole(),
                 user.getProvider() != null ? user.getProvider().name() : "LOCAL",
-                user.getProfile() != null ? ProfileFullView.from(user.getProfile()) : null
+                user.getProfile() != null ? ProfileFullView.from(user.getProfile()) : null,
+                user.getStatus() != null ? user.getStatus() : null,
+                user.getCreatedAt(),
+                user.getUpdatedAt()
         );
     }
 }
