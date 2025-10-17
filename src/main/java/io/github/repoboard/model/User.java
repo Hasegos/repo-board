eb6dfed7fdfa76cdb408da08b0248818c73f1d95
Hasegos <csw020106@naver.com>
2025-10-17 16:20:28 +0900

    FIX User 엔티티 수정
    불필요한 import 삭제

--- src/main/java/io/github/repoboard/model/User.java
+++ src/main/java/io/github/repoboard/model/User.java
@@ -9,10 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
 
-import java.time.Instant;
 import java.util.List;
 
 /**
